377e2fe172aeb058e80fd823ac0a2fc98f84594b
Ankit Bhardwaj <bhardwaj.ankit275@gmail.com>
2023-03-05 21:09:58 -0500

    New Video

--- timelapse_images.py
+++ timelapse_images.py
@@ -77,60 +77,74 @@ burst_input_rate = {'AD':v, 'BD':v, 'CD':v, 'EF':v, 'GH':v, 'IJ':v}
 i1, i2, i3 = 500, 500, 500
 tot_i = i1+i2+i3
 
-S = simulator(edges, lengths, input_edges, next_edge, merges, curve)
-for k in tqdm(range(tot_i)):
-    
-    if(k<i1 or k>=i1+i2):
-        input_rate = base_input_rate
-    else:
-        input_rate = burst_input_rate
-    
-    S.iteration_self_regulate(input_rate)
-    buff = {}
-    for edge in list(S.buffers.keys()):
-        buff[edge] = len(S.buffers[edge].queue)/(S.buffers[edge].maxsize*1.0)
 
-    fig = plt.figure(layout='constrained', figsize=(10, 8))
-    ax = fig.subplots(2, 2)
+sign = {'no':'No Signaling', 'eq': 'Equal-Time Signaling', 'bp': 'Backpressure Signaling'}
+num = 0
 
-    ax_top = ax[0,0]
-    img = mpimg.imread('topology.png')
-    ax_top.imshow(img)
-    ax_top.tick_params(which='both',bottom=False,left=False,top=False,right=False,labelbottom=False,labelleft=False)
-    ax_top.set_title('Topology',size=18)
+for signal in ['no', 'eq', 'bp']:
+    S = simulator(edges, lengths, input_edges, next_edge, merges, curve)
+    for k in tqdm(range(tot_i)):
+        if(k<i1 or k>=i1+i2):
+            input_rate = base_input_rate
+        else:
+            input_rate = burst_input_rate
 
-    ax_in = ax[0,1]
-    x = list(input_rate.values())
-    y = input_edges
-    ax_in.bar(y,x,align='center',width=0.5)
-    ax_in.axis(ymin=0,ymax=0.22)
-    ax_in.set_xlabel('Input Edges', size=14)
-    ax_in.set_ylabel('Input Rate (cars/sec)', size=14)
-    ax_in.axhline(y=0.088,label='base rate',c='g')
-    ax_in.axhline(y=0.19,label='burst rate',c='r')
-    ax_in.legend()
-    ax_in.set_title('Input Rates',size=18)
+        try:
+            if(signal == 'bp'):
+                S.iteration_self_regulate(input_rate)
+            if(signal == 'no'):
+                S.iteration_nosignal(input_rate)
+            if(signal == 'eq'):
+                S.iteration_eqsignal(input_rate)
+                
+            num+=1
+            buff = {}
+            for edge in list(S.buffers.keys()):
+                buff[edge] = len(S.buffers[edge].queue)/(S.buffers[edge].maxsize*1.0)
 
-    ax_b = ax[1,0]       
-    x = list(buff.values())
-    y = edges
-    ax_b.axis(xmin=0,xmax=1)
-    ax_b.barh(y,x,align='center')
-    ax_b.set_xlabel('Buffer Capacity',size=14)
-    ax_b.set_ylabel('Edges',size=14)
-    ax_b.axvline(x=0.33, label='b*')
-    ax_b.legend()
-    ax_b.set_title('Buffer Capacities',size=18)
+            fig = plt.figure(layout='constrained', figsize=(10, 8))
+            ax = fig.subplots(2, 2)
 
-    ax_c = ax[1,1]
-    ax_c.plot(curve[:,0],curve[:,1],lw=2)
-    ax_c.set_xlabel('Buffer Density',size=14)
-    ax_c.set_ylabel('Exit Rate (#cars/second)',size=14)
-    ax_c.set_title('Exit Rates',size=18)
-    for edge in list(S.buffers.keys()):
-        ax_c.scatter(buff[edge],curve[curve[:,0]==buff[edge]][0][1],label=edge)
-    ax_c.legend()
+            ax_top = ax[0,0]
+            img = mpimg.imread('topology.png')
+            ax_top.imshow(img)
+            ax_top.tick_params(which='both',bottom=False,left=False,top=False,right=False,labelbottom=False,labelleft=False)
+            ax_top.set_title('Topology',size=18)
 
-    fig.suptitle('Time '+str(S.time)+' s',size=24)
-    fig.savefig('images_for_video/t_{}.png'.format(S.time))
-    plt.close(fig)
+            ax_in = ax[0,1]
+            x = list(input_rate.values())
+            y = input_edges
+            ax_in.bar(y,x,align='center',width=0.5)
+            ax_in.axis(ymin=0,ymax=0.22)
+            ax_in.set_xlabel('Input Edges', size=14)
+            ax_in.set_ylabel('Input Rate (cars/sec)', size=14)
+            ax_in.axhline(y=0.088,label='base rate',c='g')
+            ax_in.axhline(y=0.19,label='burst rate',c='r')
+            ax_in.legend()
+            ax_in.set_title('Input Rates',size=18)
+
+            ax_b = ax[1,0]       
+            x = list(buff.values())
+            y = edges
+            ax_b.axis(xmin=0,xmax=1)
+            ax_b.barh(y,x,align='center')
+            ax_b.set_xlabel('Buffer Capacity',size=14)
+            ax_b.set_ylabel('Edges',size=14)
+            ax_b.axvline(x=0.33, label='b*')
+            ax_b.legend()
+            ax_b.set_title('Buffer Capacities',size=18)
+
+            ax_c = ax[1,1]
+            ax_c.plot(curve[:,0],curve[:,1],lw=2)
+            ax_c.set_xlabel('Buffer Density',size=14)
+            ax_c.set_ylabel('Exit Rate (#cars/second)',size=14)
+            ax_c.set_title('Exit Rates',size=18)
+            for edge in list(S.buffers.keys()):
+                ax_c.scatter(buff[edge],curve[curve[:,0]==buff[edge]][0][1],label=edge)
+            ax_c.legend()
+
+            fig.suptitle(sign[signal]+' Time '+str(S.time)+' s',size=24)
+            fig.savefig('images_for_video/{}.png'.format(num))
+            plt.close(fig)
+        except:
+            break
